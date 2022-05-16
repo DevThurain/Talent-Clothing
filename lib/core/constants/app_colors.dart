af9b39910fbaba25994d6488464d6d16ecaebe8a
thurain <thurainbowin.dev@gmail.com>
2022-05-16 21:21:18 +0630

    - add color theme

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const black = Color(0xff000000);
+  static const white = Color(0xffffffff);
+  static const makeup = Color(0xffFA9E69);
+  static const darkGrey = Color(0xffCFCBCA);
+  static const grey = Color(0xffCFCBCA);
+}
